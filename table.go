ee96388cbe82e334b16e43fb45b8118694ff7846
Danny Hermes <dhermes@blend.com>
2020-08-18 22:27:17 -0500

    Adding `CreateMigrationsTable()` helper.

---
+++ table.go
@@ -0,0 +1,53 @@
+package golembic
+
+import (
+	"context"
+	"database/sql"
+	"log"
+)
+
+const (
+	// DefaultMetadataTableName is the default name for the table used to store
+	// metadata about migrations.
+	DefaultMetadataTableName = "golembic_migrations"
+
+	// TODO: https://github.com/dhermes/golembic/issues/2
+	createMigrationsTableSQL = `
+CREATE TABLE golembic_migrations (
+    id SERIAL,
+    revision VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+`
+	addPrimaryKeyMigrationsTableSQL = `
+ALTER TABLE golembic_migrations ADD CONSTRAINT pk_golembic_migrations_id PRIMARY KEY (id);
+`
+)
+
+func rollbackAndLog(tx *sql.Tx) {
+	err := tx.Rollback()
+	if err == nil || err == sql.ErrTxDone {
+		return
+	}
+
+	// TODO: https://github.com/dhermes/golembic/issues/1
+	log.Println(err)
+}
+
+// CreateMigrationsTable invokes SQL statements required to create the metadata
+// table used to track migrations.
+func CreateMigrationsTable(ctx context.Context, tx *sql.Tx) error {
+	defer rollbackAndLog(tx)
+
+	_, err := tx.ExecContext(ctx, createMigrationsTableSQL)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.ExecContext(ctx, addPrimaryKeyMigrationsTableSQL)
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
