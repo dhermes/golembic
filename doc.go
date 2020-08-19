a1b2df1bad2f4c4ca5cd153090484b07c3e33ba0
Danny Hermes <dhermes@blend.com>
2020-08-19 15:57:09 -0500

    Dedent godoc.

--- doc.go
+++ doc.go
@@ -3,20 +3,20 @@
 // The underlying principles of golembic are as follows:
 //
 // - Migrations should follow a straight line (from some root); this line should
-//   be verified to avoid merge conflicts causing "branching"
+// be verified to avoid merge conflicts causing "branching"
 //
 // - The "current" state of the world will be tracked in a `migrations` table
-//   containing all migrations that have been applied.
+// containing all migrations that have been applied.
 //
 // - A series of migrations should be easy to use both in a script or as part
-//   of a larger piece of Go code
+// of a larger piece of Go code
 //
 // - Avoid all import time side effects caused either by importing a package that
-//   uses `init()` or by requiring migrations files to use `init()`
+// uses `init()` or by requiring migrations files to use `init()`
 //
 // - Down migrations are not supported. The idea being that the risk of data loss
-//   from a down migration is not worth it and that writing down migrations can
-//   be more challening than writing up migrations.
+// from a down migration is not worth it and that writing down migrations can
+// be more challening than writing up migrations.
 //
 // The design allows for running "arbitrary" code inside migrations
 // so that even non-SQL tasks can be tracked as a "run-once" migration.
