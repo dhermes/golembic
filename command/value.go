26df496871b77ad576d14ae15bedfd9f10f7e0e2
Danny Hermes <dhermes@blend.com>
2020-11-10 12:04:13 -0600

    Rename `RoundDuration` receiver from `dv` to `rd`.
    Fixes #19.

--- command/value.go
+++ command/value.go
@@ -24,31 +24,31 @@ type RoundDuration struct {
 }
 
 // String is the string representation of the stored value.
-func (dv *RoundDuration) String() string {
-	if dv.Value == nil || *dv.Value == 0 {
+func (rd *RoundDuration) String() string {
+	if rd.Value == nil || *rd.Value == 0 {
 		return ""
 	}
-	return fmt.Sprintf("%s", dv.Value)
+	return fmt.Sprintf("%s", rd.Value)
 }
 
 // Set sets the duration based on a string input.
-func (dv *RoundDuration) Set(value string) error {
+func (rd *RoundDuration) Set(value string) error {
 	d, err := time.ParseDuration(value)
 	if err != nil {
 		return err
 	}
 
-	_, err = golembic.ToRoundDuration(d, dv.Base)
+	_, err = golembic.ToRoundDuration(d, rd.Base)
 	if err != nil {
 		return err
 	}
 
-	*dv.Value = d
+	*rd.Value = d
 	return nil
 }
 
 // Type is a human readable "description" of the underlying type being
 // represented.
-func (dv *RoundDuration) Type() string {
+func (*RoundDuration) Type() string {
 	return "duration"
 }
